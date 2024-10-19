7bb99062b27038d8bc421ad02ecf848f22cc4cf2
Utsav Saxena <utsavmra2004@gmail.com>
2024-10-19 23:22:05 +0530

    19 oct

--- 19Oct.cpp
+++ 19Oct.cpp
@@ -25,3 +25,29 @@ void insert(int arr[], int i,int n)
         }
         insert(arr,i+1,n);
     }
+
+//31 Next Permutation
+void nextPermutation(vector<int>& nums) {
+        // int size= nums.size();
+        // next_permutation(nums.begin(),nums.end());
+        int idx = -1;
+        int n = nums.size();
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                idx = i;
+                break;
+            }
+        }
+        if(idx==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        reverse(nums.begin()+idx+1,nums.end());
+        for(int i = idx+1;i<n;i++){
+            if(nums[i] > nums[idx]){
+                swap(nums[i],nums[idx]);
+                break;
+            }
+        }
+        
+    }
